e14f423ac0a80930b9dff9ad5cefe723a0d760d0
IBAD ULLAH KHAN <ibad50008@gmail.com>
2026-02-26 12:29:02 +0500

    changes is made

---
+++ Client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  
+
+  return (
+   <>
+   <h1 className='bg-amber-700'>Hello Mern satck  Project </h1>
+   </>
+  )
+}
+
+export default App
