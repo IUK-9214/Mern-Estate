96f84335588f0edc1cbf8c78682e9f5e784f9238
IBAD ULLAH KHAN <ibad50008@gmail.com>
2026-02-26 12:45:21 +0500

    routing is set in the App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,14 +1,25 @@
 import { useState } from 'react'
-
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Signin from './pages/Signin'
+import About from './pages/About'
+import SignUp from './pages/SignUp'
+import Profile from './pages/Profile'
 
 function App() {
   
 
-  return (
-   <>
-   <h1 className='bg-amber-700'>Hello Mern satck  Project </h1>
-   </>
-  )
+  return <BrowserRouter>
+  <Routes>
+<Route path='/' element={<Home/>}/>
+<Route path='/sign-in' element={<Signin/>}/>
+<Route path='/sign-up' element={<SignUp/>}/>
+<Route path='/about' element={<About/>}/>
+<Route path='/profile' element={<Profile/>}/>
+
+  </Routes>
+  </BrowserRouter>
+  
 }
 
 export default App
