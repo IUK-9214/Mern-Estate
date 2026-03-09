b238bad0627a7347979c4de7bf29ebb01b4ef602
IBAD ULLAH KHAN <ibad50008@gmail.com>
2026-03-09 20:29:05 +0500

    signup page ui is completed

--- Client/src/Component/Header.jsx
+++ Client/src/Component/Header.jsx
@@ -20,6 +20,7 @@ function Header() {
                     <Link to={'/'}><li className='hidden sm:inline text-slate-700 hover:underline cursor-pointer'>Home</li></Link>
                     <Link to={'/about'}>   <li className='hidden sm:inline text-slate-700 hover:underline cursor-pointer'>About</li></Link>
                     <Link to={'/sign-in'}>   <li className=' text-slate-700 hover:underline cursor-pointer'>Sign in</li></Link>
+                    <Link to={'/sign-up'}>   <li className=' text-slate-700 hover:underline cursor-pointer'>Sign Up</li></Link>
                 </ul>
             </div>
         </header>
