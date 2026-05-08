e4db033cb9525dd80fef4b766f0d732689018a83
IBAD ULLAH KHAN <ibad50008@gmail.com>
2026-05-09 00:36:33 +0500

    error handling was set in the signup also the path error was elimenated

--- Client/src/pages/SignUp.jsx
+++ Client/src/pages/SignUp.jsx
@@ -15,17 +15,27 @@ function SignUp() {
   }
 const HandleSubmit=async(e)=>{
   e.preventDefault();
-  setLoading(true)
-const res =await fetch('/api/auth/signup',{
+ try {
+   setLoading(true)
+const res =await fetch('/api/signup',{
   method:'POST',
   headers:{
-    'Contest-Type':'application/json',
+    'Content-Type':'application/json',
   },
   body:JSON.stringify(FormData),
 });
 const data=await res.json();
+if(data.success===false){
+  setLoading(false)
+  setError(data.message);
+  return;
+}
+setLoading(false)
 
-
+ } catch (error) {
+  setLoading(false);
+  setError(error.message); 
+ }
 }
 
   return (
@@ -33,17 +43,18 @@ const data=await res.json();
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
       <form
-      
+      onSubmit={HandleSubmit}
       className='flex flex-col gap-4'>
         <input  onChange={HandleChange}  type="text" placeholder='UserName' className='border p-3 rounded-lg' id='username' />
         <input onChange={HandleChange}  type="email" placeholder='Email' className='border p-3 rounded-lg' id='email' />
         <input  onChange={HandleChange} type="password" placeholder='Password' className='border p-3 rounded-lg' id='password' />
-        <button className='bg-slate-700 text-white p-3 rounded-lg  uppercase hover:opacity-95 disabled::opacity-80'>sign up</button>
+        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg  uppercase hover:opacity-95 disabled::opacity-80'>{loading?"Loading...":"Singup"}</button>
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Have an account ? </p>
         <Link className='text-blue-700' to={"/sign-in"}> Sign In</Link>
       </div>
+      
     </div>
   )
 }
