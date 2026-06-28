8f6fe5c66571fb40f97ed717dc95c56d60d2924d
IBAD ULLAH KHAN <ibad50008@gmail.com>
2026-06-28 17:34:22 +0500

    updated header search form

--- Client/src/Component/Header.jsx
+++ Client/src/Component/Header.jsx
@@ -1,54 +1,79 @@
-import { FaSearch } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import { FaSearch } from 'react-icons/fa';
+import { Link, useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 
-function Header() {
+export default function Header() {
+ 
+  const { currentuser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
 
-  const { currentuser } = useSelector(state => state.user)  
-    return (
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [window.location.search]);
+
+  return (
     <header className='bg-slate-200 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>SoloTech</span>
-            <span className='text-slate-700'>Company</span>
+            <span className='text-slate-500'>Sahand</span>
+            <span className='text-slate-700'>Estate</span>
           </h1>
         </Link>
-
-        <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+        <form
+          onSubmit={handleSubmit}
+          className='bg-slate-100 p-3 rounded-lg flex items-center'
+        >
           <input
-            type="text"
+            type='text'
             placeholder='Search...'
-            className='bg-transparent focus:outline-none w-24 sm:w-64' />
-          <FaSearch className='text-slate-600' />
+            className='bg-transparent focus:outline-none w-24 sm:w-64'
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <button>
+            <FaSearch className='text-slate-600' />
+          </button>
         </form>
-
-        <ul className='flex gap-4 items-center'>
+        <ul className='flex gap-4'>
           <Link to='/'>
-            <li className='hidden sm:inline text-slate-700 hover:underline cursor-pointer'>Home</li>
+            <li className='hidden sm:inline text-slate-700 hover:underline'>
+              Home
+            </li>
           </Link>
           <Link to='/about'>
-            <li className='hidden sm:inline text-slate-700 hover:underline cursor-pointer'>About</li>
+            <li className='hidden sm:inline text-slate-700 hover:underline'>
+              About
+            </li>
           </Link>
           <Link to='/profile'>
+          
             {currentuser ? (
               <img
                 className='rounded-full h-7 w-7 object-cover'
-                src={currentuser.avatar}        
-                alt="profile pic"
-                onError={(e) => {               
-                  e.target.src = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
-                }}
+                src={currentuser.avatar}
+                alt='profile'
               />
             ) : (
-              <li className='text-slate-700 hover:underline cursor-pointer'>Sign in</li>
+              <li className='text-slate-700 hover:underline'>Sign in</li>
             )}
           </Link>
         </ul>
-
       </div>
     </header>
-  )
+  );
 }
-
-export default Header
